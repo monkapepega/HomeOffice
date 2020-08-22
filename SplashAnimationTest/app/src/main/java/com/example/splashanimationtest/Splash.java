ad78745a000697664a9dad20187361e981b23dce
siang <foosiang_94@hotmail.com>
2020-08-22 21:30:38 +0200

    Home Office

---
+++ SplashAnimationTest/app/src/main/java/com/example/splashanimationtest/Splash.java
@@ -0,0 +1,34 @@
+package com.example.splashanimationtest;
+
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+    Animation anim;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        imageView=(ImageView)findViewById(R.id.imageView2); // Declare an imageView to show the animation.
+        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in); // Create the animation.
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(this,HomeActivity.class));
+                // HomeActivity.class is the activity to go after showing the splash screen.
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        imageView.startAnimation(anim);
+    }
+}
---
+++ TabbedLayoutTest/app/src/main/java/com/example/tabbedlayouttest/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.tabbedlayouttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabBar);
+        TabItem tabChats = findViewById(R.id.tabChats);
+        TabItem tabStatus = findViewById(R.id.tabStatus);
+        TabItem tabCalls = findViewById(R.id.tabCalls);
+        final ViewPager viewPager = findViewById(R.id.viewPager);
+
+        PagerAdapter pagerAdapter = new
+                PagerAdapter(getSupportFragmentManager(),
+                    tabLayout.getTabCount()) ;
+
+        viewPager.setAdapter(pagerAdapter);
+
+        //One last step is to change the tabs view when the tab is selected or clicked
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
